c8d532b593a08e60277dd69abc869aac04e3db5a
IbrahimAlobaid22 <ibrahim.alobid39@gmail.com>
2024-12-20 22:21:18 +0300

    update instructions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
 app = FastAPI()
-@app.get("/welcome")
-def welcome():
-    return {
-        'massage' : 'Hello world!'
-    }
+app.include_router(base.base_router)
