6839c8f40159431f7790f20dda84b1c8b9df165a
m-rzini <mohammed.rzini@yahoo.fr>
2025-07-15 12:21:23 +0100

    init projet directory

--- src/models/db_schemes/minirag/schemes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .minirag_base import SQLAlchemyBase
-from .asset import Asset
-from .project import Project
-from .datachunk import DataChunk, RetrievedDocument
--- src/routes/schemes/nlp.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class PushRequest(BaseModel):
-    do_reset: Optional[int] = 0
-
-class SearchRequest(BaseModel):
-    text: str
-    limit: Optional[int] = 5
