d6d5181311440be7483ad33d9c74437c0a5a9e8c
Mazen Ahmed <mazen.fayd@gmail.com>
2025-11-29 11:52:24 +0200

    move app files into src directory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")  # Load environment variables from .env file
+from routes.base import base_router
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome_message():
-    return {
-        "message": "Welcome to the FastAPI application!"    
-    }
+app.include_router(base_router)
