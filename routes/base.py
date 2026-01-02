8d0463799446df6666d99623e14bb278722f1e12
Mohammed Elshenawy <MElshenawy@healthinsightsgroup.com>
2026-01-02 19:01:47 +0200

    add welcome routes

--- routes/base.py
+++ routes/base.py
@@ -6,7 +6,7 @@ base_router = APIRouter(
     tags=["api_v1"],
 )
 
-@base_router.get("/")
+@base_router.get("/welcome")
 async def welcome():
     app_name = os.getenv('APP_NAME')
     app_version = os.getenv('APP_VERSION')
