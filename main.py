ce53f409c95a2aa817e2f476255a59ef3fe1578c
Experimental-1 <experimentalunit729@gmail.com>
2026-01-07 04:54:54 +0300

    Moved all logic files into the src folder

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv(".env")
-from routes import base #from routes the folder import base.py
-
-app = FastAPI()
-app.include_router(base.base_router)
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,10 +1,5 @@
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI,APIRouter
 import os
-from helpers.config import get_settings, Settings
-from time import sleep
-import logging
-
-logger = logging.getLogger('uvicorn.error')
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -12,12 +7,11 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome(app_settings: Settings = Depends(get_settings)):
-
-    app_name = app_settings.APP_NAME
-    app_version = app_settings.APP_VERSION
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
 
     return {
-        "app_name": app_name,
-        "app_version": app_version,
+        "app_name":app_name,
+        "app_version":app_version,
     }
