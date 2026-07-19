225510ef487a32a096ea40d872260215dba41dc7
fouad chaouki <127513023+Fch-3388@users.noreply.github.com>
2026-07-19 14:05:22 +0100

    feat: modularize API routes by initializing base router and refactoring entry point structure

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from routes import base
+
 
 load_dotenv(".env")
 
+from routes import base
 app = FastAPI()
 
 app.include_router(base.base_router)
